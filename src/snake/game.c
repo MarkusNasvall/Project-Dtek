48684a5dad9999d389a2c3c400e2e1f0af76f57d
MohaBars <mohomeister@gmail.com>
2023-11-26 17:53:29 +0100

    add labwork.s for the delay func

--- src/snake/game.c
+++ src/snake/game.c
@@ -1,14 +1,8 @@
-/* mipslabwork.c
+/* game.c
 
-   This file written 2015 by F Lundevall
-   Updated 2017-04-21 by F Lundevall
-
-   This file should be changed by YOU! So you must
-   add comment(s) here with your name(s) and date(s):
-
-   This file modified 2017-04-31 by Ture Teknolog 
-
-   For copyright and licensing, see file COPYING */
+   This file written 2023 by M. Ba Rashd and M. Näsvall
+    
+    */
 
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include "pic32mx.h"  /* Declarations of system-specific addresses etc */
